e686352fc136d57a996a015ab628a7acc7f26eb0
Stephen Farmer <safarmer@icloud.com>
2023-02-23 22:45:43 +0000

    add code example

--- src/main/java/generics/typeerasure/Main.java
+++ src/main/java/generics/typeerasure/Main.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Oklahoma");
-        String state = list.get(0);
-        
+
+        // Uses generics correctly, then translates it into the equivalent code below
+        ArrayList<String> stateList = new ArrayList<>();
+        stateList.add("Oklahoma");
+        String state = stateList.get(0);
+        System.out.println(state);
+
+
+        // For runtime use, this code uses the raw type
+        ArrayList cityList = new ArrayList();
+        cityList.add("Detroit");
+        String city = (String)(cityList.get(0));
+        System.out.println(city);
     }
 }
